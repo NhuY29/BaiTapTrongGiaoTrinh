2bf3c2f0aea32f233437b57055d3a72bc9da3502
NhuY296 <ny22167@gmail.com>
2022-02-27 17:42:06 +0700

    Làm xong bài tập 1 trong sách giao trình nhập môn lập trình

--- BaiTapTrongGiaoTrinh/BaiTapTrongGiaoTrinh.cpp
+++ BaiTapTrongGiaoTrinh/BaiTapTrongGiaoTrinh.cpp
@@ -1,15 +1,42 @@
 // BaiTapTrongGiaoTrinh.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <stdio.h>
 #include <iostream>
+
+int inHoVaTen() {
+    char hoVaTen[30];
+    printf("Nhap ho va ten cua ban va Enter: ");
+    // gets(hoVaTen); Tranh dung gets vi rat nguy hiem se gay loi tran bo nho dem
+    getchar(); // Goi getchar() truoc fgets(), gets() vi fgets(), gets() nhan luon phim enter o phan truoc nen ham fgets() se bi bo qua
+    fgets(hoVaTen,sizeof(hoVaTen), stdin); // Su dung fgets() de khong bi loi tran bo nho dem
+    printf("Xin chao ban %s\n", hoVaTen);
+    printf("Chuc ban %s hoc tot!!", hoVaTen);
+    return 11; // Thuc hien bai 1 chuong 1 
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int phim = 0;
+    printf("Chao mung ban da den voi bai tap cua tui\n");
+    printf("Vui long nhan phim 1 de nhap vao ho ten\n");
+    scanf_s("%i", &phim);
+    int trangThaiBai = 0;
+    switch (phim)
+    {
+    case 1:
+        trangThaiBai = inHoVaTen();
+        break;
+    default:
+        break;
+    }
+    return 0;
+
+
 }
 
+
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu 
 
 // Tips for Getting Started: 
 //   1. Use the Solution Explorer window to add/manage files
